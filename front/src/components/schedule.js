b7ddf278f53a5749194b4cf5492073f01109492c
AinhoaMaya <ainhoamaya.web@gmail.com>
2024-03-06 13:53:42 +0100

    Cambios en "schedule"

--- front/src/components/schedule.js
+++ front/src/components/schedule.js
@@ -33,7 +33,78 @@ class Horario extends HTMLElement {
       title: 'encuentro mosaico',
       subtitle: 'Un espacio para compartir, aprender y disfrutar',
       date: 'Llucmajor | 09:00h',
-      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae'
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae',
+      text: '¡Apúntate a las actividades!',
+      schedule: [
+        {
+          title: 'Nuevas tecnologías y educación',
+          date: '09:00h - 10:00h',
+          size: 'frame-1x1',
+          color: 'hsl(146deg 50% 45%)'
+        },
+        {
+          title: 'Nuevas tecnologías y educación',
+          date: '10:30h - 11:30h',
+          size: 'frame-1x1',
+          color: 'hsl(146deg 50% 45%)'
+        },
+        {
+          title: 'Nuevas tecnologías y educación',
+          date: '11:15h - 11:45h',
+          size: 'frame-1x1',
+          color: 'hsl(146deg 50% 45%)'
+        },
+        {
+          title: 'Nuevas tecnologías y educación',
+          date: '12:00h - 12:30h',
+          size: 'frame-1x1',
+          color: 'hsl(146deg 50% 45%)'
+        },
+        {
+          title: 'Nuevas tecnologías y educación',
+          date: '12:20h - 13:00h',
+          size: 'frame-1x1',
+          color: 'hsl(146deg 50% 45%)'
+        },
+        {
+          title: 'Nuevas tecnologías y educación',
+          date: '12:45h - 13:45h',
+          size: 'frame-1x1',
+          color: 'hsl(146deg 50% 45%)'
+        },
+        {
+          title: 'Nuevas tecnologías y educación',
+          date: '13:15h - 13:50h',
+          size: 'frame-1x1',
+          color: 'hsl(146deg 50% 45%)'
+        },
+        {
+          title: 'Nuevas tecnologías y educación',
+          date: '13:30h - 14:00h',
+          size: 'frame-1x1',
+          color: 'hsl(146deg 50% 45%)'
+        },
+        {
+          title: 'Nuevas tecnologías y educación',
+          date: '13:30h - 14:00h',
+          size: 'frame-1x1',
+          color: 'hsl(146deg 50% 45%)'
+        },
+        {
+          title: 'Nuevas tecnologías y educación',
+          date: '13:30h - 14:00h',
+          size: 'frame-1x1',
+          color: 'hsl(146deg 50% 45%)'
+        }
+      ],
+      scheduleDinner: [
+        {
+          title: 'Hora de la comida y merienda',
+          date: 'Comida: 14:00h - 14:45h | Merienda: 16:00h - 16:30h',
+          size: 'frame-1x1',
+          color: 'hsl(248deg 53% 57%)'
+        }
+      ]
     }
   }
 
@@ -99,21 +170,6 @@ class Horario extends HTMLElement {
             padding: 0 0 2rem 0;
           }
 
-          .schedule-description-inscription{
-            display: flex;
-            justify-content: center;
-          }
-
-          .schedule-description-inscription h3{
-            color: hsl(209deg 100% 55%);
-          }
-          
-          .schedule-inscription-svg svg{
-            width: 2.3rem;
-            height: 2.3rem;
-            fill: hsl(209deg 100% 55%);
-          }
-
           .schedule-description p{
             font-weight: 600;
             line-height: 1.2;
@@ -121,6 +177,10 @@ class Horario extends HTMLElement {
             text-align: center;
             text-align: justify;
             padding: 0 7rem 2rem 7rem;
+          }
+
+          .schedule-inscription-title h3{
+            color: hsl(209deg 100% 55%);
           }
 
           .schedule{
@@ -137,34 +197,11 @@ class Horario extends HTMLElement {
           .frame-1x1{
             grid-column: span 1;
             grid-row: span 1;
-          }
-
-          .frame-2x2{
-            grid-column: span 2;
-            grid-row: span 2;
-          }
-
-          .frame-1x2{
-            grid-column: span 1;
-            grid-row: span 2;
-          }
-
-          .frame-2x1{
-            grid-column: span 2;
-            grid-row: span 1;
-          }
-
-          .frame-3x1{
-            grid-column: span 3;
-            grid-row: span 1;
-          }
-
-          .frame-1x1, .frame-2x2, .frame-1x2, .frame-2x1, .frame-3x1 {
             place-items: center;
             position: relative;
           }
 
-          .add-button-container {
+          .add-button-container{
             position: absolute;
             top: 0;
             right: 0;
@@ -184,155 +221,84 @@ class Horario extends HTMLElement {
         <div class="schedule-body">
           <div class="schedule-image">
             <picture>
-              <source srcset="./public/schedule.webp" type="image/webp" media="(max-width: 600px)">
-              <source srcset="./public/schedule.webp" type="image/webp" media="(min-width: 601px)">
-              <img src="./public/schedule.webp" alt="son-sampol" title="finca son sampol">
+              <source srcset="${this.data.images.lg.src}" type="image/webp" media="(min-width: 1200px)">
+              <source srcset="${this.data.images.md.src}" type="image/webp" media="(min-width: 992px)">
+              <source srcset="${this.data.images.sm.src}" type="image/webp" media="(max-width: 601px)">
+              <source srcset="${this.data.images.xs.src}" type="image/webp" media="(max-width: 600px)">
+              <img src="${this.data.images.lg.src}" alt="${this.data.images.alt}" title="${this.data.images.title}">
             </picture>
           </div>
 
           <div class="schedule-description">
-            <h1>Encuentro Mosaico</h1>
-            <h3>Un espacio para compartir, aprender y disfrutar</h3>
-            <h3>Llucmajor | 09:00h</h3>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa eos voluptate maxime, vitae, commodi amet at recusandae error saepe perspiciatis ducimus, fugiat doloremque temporibus accusamus ratione quisquam atque ipsam repudiandae</p>
-            <div class="schedule-description-inscription">
-              <div class="schedule-inscription-title">
-                <h3>¡Apúntate a las actividades!</h3>
-              </div>
-              <div class="schedule-inscription-svg">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>arrow-down</title><path d="M11,4H13V16L18.5,10.5L19.92,11.92L12,19.84L4.08,11.92L5.5,10.5L11,16V4Z" /></svg>
-              </div>
+            <h1>${this.data.title}</h1>
+            <h3>${this.data.subtitle}</h3>
+            <h3>${this.data.date}</h3>
+            <p>${this.data.description}</p>
+            <div class="schedule-inscription-title">
+              <h3>${this.data.text}</h3>
             </div>
           </div>
 
-
-          <div class="schedule">
-            <div class="schedule frame-1x1" style="background-color:hsl(9deg 100% 65%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>09:00h - 10:00h</h2>
-              </div>
-            </div>
-            <div class="schedule frame-1x1" style="background-color:hsl(38deg 100% 50%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>10:30h - 11:30h</h2>
-              </div>
-            </div>
-            <div class="schedule frame-1x1" style="background-color:hsl(209deg 100% 55%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>11:15h - 11:45h</h2>
-              </div>
-            </div>
-            <div class="schedule frame-1x1" style="background-color:hsl(146deg 50% 45%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>12:00h - 12:30h</h2>
-              </div>
-            </div>
-          </div>
-
-          <div class="schedule">
-            <div class="schedule frame-1x1" style="background-color:hsl(38deg 100% 50%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>12:20h - 13:00h</h2>
-              </div>
-            </div>
-            <div class="schedule frame-1x1" style="background-color:hsl(9deg 100% 65%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>12:45h - 13:45h</h2>
-              </div>
-            </div>
-            <div class="schedule frame-1x1" style="background-color:hsl(146deg 50% 45%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>13:15h - 13:50h</h2>
-              </div>
-            </div>
-            <div class="schedule frame-1x1" style="background-color:hsl(209deg 100% 55%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>13:30h - 14:00h</h2>
-              </div>
-            </div>
-          </div>
-
-          <div class="schedule">
-            <div class="schedule frame-1x1" style="background-color:hsl(248deg 53% 57%)">
-              <div class="schedule-info-dinner">
-                <h2>Hora de la comida y merienda</h2>
-                <h2>Comida: 14:00h - 14:45h | Merienda: 16:00h - 16:30h</h2>
-              </div>
-            </div>
-          </div>
-
-          <div class="schedule">
-            <div class="schedule frame-1x1" style="background-color:hsl(9deg 100% 65%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>15:00h - 16:00h</h2>
-              </div>
-            </div>
-            <div class="schedule frame-1x1" style="background-color:hsl(38deg 100% 50%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>16:30h - 17:30h</h2>
-              </div>
-            </div>
-            <div class="schedule frame-1x1" style="background-color:hsl(209deg 100% 55%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>17:30h - 18:00h</h2>
-              </div>
-            </div>
-            <div class="schedule frame-1x1" style="background-color:hsl(146deg 50% 45%)">
-              <div class="add-button-container">
-                <add-button-component></add-button-component>
-              </div>
-              <div class="schedule-info">
-                <h3>Nuevas tecnologías y educación</h3>
-                <h2>18:00h - 18:30h</h2>
-              </div>
-            </div>
-          </div>
+          <div class="schedule"></div>
+          <div class="schedule-dinner"></div>
         </div>
       `
+    const schedules = this.shadow.querySelector('.schedule')
+    const schedulesDinner = this.shadow.querySelector('.schedule-dinner')
+
+    this.data.schedule.forEach(category => {
+      const schedule = document.createElement('div')
+      schedule.classList.add('schedule')
+      schedule.classList.add(category.size)
+      schedules.appendChild(schedule)
+      schedule.style.backgroundColor = category.color
+
+      const buttonContainer = document.createElement('div')
+      buttonContainer.classList.add('add-button-container')
+      schedule.appendChild(buttonContainer)
+
+      const addButton = document.createElement('add-button-component')
+      buttonContainer.appendChild(addButton)
+
+      const scheduleInfo = document.createElement('div')
+      scheduleInfo.classList.add('schedule-info')
+      schedule.appendChild(scheduleInfo)
+
+      const scheduleTitle = document.createElement('h3')
+      scheduleTitle.textContent = category.title
+      scheduleInfo.appendChild(scheduleTitle)
+
+      const scheduleDate = document.createElement('h2')
+      scheduleDate.textContent = category.date
+      scheduleInfo.appendChild(scheduleDate)
+    })
+
+    this.data.scheduleDinner.forEach(category => {
+      const scheduleDinner = document.createElement('div')
+      scheduleDinner.classList.add('schedule')
+      scheduleDinner.classList.add(category.size)
+      schedulesDinner.appendChild(scheduleDinner)
+      scheduleDinner.style.backgroundColor = category.color
+
+      const buttonContainerDinner = document.createElement('div')
+      buttonContainerDinner.classList.add('add-button-container')
+      scheduleDinner.appendChild(buttonContainerDinner)
+
+      const addButtonDinner = document.createElement('add-button-component')
+      buttonContainerDinner.appendChild(addButtonDinner)
+
+      const scheduleInfoDinner = document.createElement('div')
+      scheduleInfoDinner.classList.add('schedule-info')
+      scheduleDinner.appendChild(scheduleInfoDinner)
+
+      const scheduleTitleDinner = document.createElement('h3')
+      scheduleTitleDinner.textContent = category.title
+      scheduleInfoDinner.appendChild(scheduleTitleDinner)
+
+      const scheduleDateDinner = document.createElement('h2')
+      scheduleDateDinner.textContent = category.date
+      scheduleInfoDinner.appendChild(scheduleDateDinner)
+    })
   }
 }
 
